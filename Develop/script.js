743ac3069a1bde40730256af873e68736840a46f
Alex Hageman <ahageman27@gmail.com>
2022-05-04 18:35:40 -0600

    added functionality to checkboxes and dropdown that creates list of characters needed

--- Develop/script.js
+++ Develop/script.js
@@ -14,20 +14,45 @@ for (var i = 8; i < 129; i++) {
   document.getElementById("length").appendChild(lengthElement);
 }
 
+const lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+const specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "`", "~", "|", "<", ">", "=", "-", "_"]
+
 const lowercaseCheck = document.querySelector("#lowercase");
 const uppercaseCheck = document.querySelector("#uppercase");
 const numericCheck = document.querySelector("#numeric");
 const specialCheck = document.querySelector("#special");
+var passwordCharacters = []
 
-
-
-function generatePassword() {
+function updateSelections() {
   selections.length = document.querySelector("#length").value;
   selections.lowercase = lowercaseCheck.checked;
   selections.uppercase = uppercaseCheck.checked;
   selections.numeric = numericCheck.checked;
   selections.special = specialCheck.checked;
-  console.log(selections)
+}
+
+function chooseCharacters() {
+  if (selections.lowercase) {
+    passwordCharacters = passwordCharacters.concat(lowercaseCharacters);
+  }
+  if (selections.uppercase) {
+    passwordCharacters = passwordCharacters.concat(uppercaseCharacters);
+  }
+  if (selections.numeric) {
+    passwordCharacters = passwordCharacters.concat(numericCharacters);
+  }
+  if (selections.special) {
+    passwordCharacters = passwordCharacters.concat(specialCharacters);
+  }
+}
+
+function generatePassword() {
+  updateSelections();
+  chooseCharacters();
+  console.log(selections);
+  console.log(passwordCharacters);
 }
 
 // Get references to the #generate element
